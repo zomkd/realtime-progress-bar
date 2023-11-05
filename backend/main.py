60e7004e8c0179766b4fe482967a621210bb558f
dull69 <dull69@mail.ru>
2023-11-05 15:28:10 +0300

    add websoket connection

---
+++ backend/main.py
@@ -0,0 +1,18 @@
+
+from fastapi import FastAPI, WebSocket
+
+app = FastAPI()
+
+
+# @app.get("/")
+# def root():
+#     return {"msg":"welcome"}
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+
+        data = await websocket.receive_text()
+        print(data)
+        await websocket.send_text(f"{data}")
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted,ref } from 'vue'
+
+const data = ref()
+const inputData = ref()
+const connection = new WebSocket("ws://localhost:8000/ws")
+
+
+function submit()  {
+  connection.send(inputData.value)
+}
+onMounted(() => {
+
+  connection.onmessage = function(e){
+
+    data.value = e.data
+
+  }
+
+})
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+  <h1>hello {{data}}</h1>
+  <input type="text" v-model="inputData" @keyup.enter="submit()">
+  <button @click="submit()">submit</button>
+  <RouterView />
+</template>
