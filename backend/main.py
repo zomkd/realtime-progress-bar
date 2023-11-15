44c390997edac162cf77299ed3d2594d3029bf7c
dull69 <dull69@mail.ru>
2023-11-15 17:03:20 +0300

    refactor backend

--- backend/main.py
+++ backend/main.py
@@ -2,15 +2,14 @@ import random
 from fastapi.middleware.cors import CORSMiddleware
 from celery.result import AsyncResult
 from celery import Celery
-from fastapi import FastAPI, WebSocket, Body
+from fastapi import FastAPI, WebSocket
 from fastapi.responses import JSONResponse
 from time import sleep
  
 app = FastAPI()
+
 origins = [
     "*",
-    "http://localhost",
-    "http://localhost:5173",
 ]
 
 app.add_middleware(
@@ -27,46 +26,28 @@ celery = Celery(
     backend="redis://127.0.0.1:6379/0"
 )
 
-@celery.task(name="create_task", bind=True)
-def create_task(self, task_name):
+@app.get("/task")
+def set_task():
+    task = run_task.delay()
+    return JSONResponse({"task_id": task.id})
+
+@celery.task(name="run_task", bind=True)
+def run_task(self):
     n = 30
     for i in range(0, n):
         self.update_state(state='PROGRESS', meta={'done': i, 'total': n})
         sleep(random.randint(1, 10))
     self.update_state(state='SUCCESS', meta={'done': n, 'total': n})
 
-@app.get("/")
-def root():
-    return {"msg":"welcome"}
-
-
-@app.post("/task")
-def set_task(payload = Body(...)):
-    task_name = payload["task_name"]
-    print(task_name)
-    task = create_task.delay(task_name)
-    return JSONResponse({"task_id": task.id})
-
 @app.websocket("/task/{task_id}")
-async def websocket_endpoint(websocket: WebSocket, task_id):
+async def task_status_listiner(websocket: WebSocket, task_id: str):
     await websocket.accept()
     task_result = AsyncResult(task_id)
-    print(task_result.state)
-    running = True
-    while running:
-        if task_result.state == 'SUCCESS':
-            running = False
-        if task_result.state == 'PROGRESS':
-            result = {
-                "task_id": task_id,
-                "task_status": task_result.state,
-                "task_result": task_result.result
-            }
-            await websocket.send_json(result)
-        else:
-            result = {
-                "task_id": task_id,
-                "task_status": task_result.state,
-                "task_result": task_result.result
-            }
-            await websocket.send_json(result)
+
+    while task_result.state != 'SUCCESS':
+        result = {
+            "task_id": task_id,
+            "task_status": task_result.state,
+            "task_result": task_result.result
+        }
+        await websocket.send_json(result)
